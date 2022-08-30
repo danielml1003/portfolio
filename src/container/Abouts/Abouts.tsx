07b4ceaa386704e17527a6c8417847d41462eb9d
danielml1003 <danielbaravik1003@gmail.com>
2022-08-30 21:51:51 +0300

    second commit

---
+++ src/container/Abouts/Abouts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./Abouts.scss"
+
+const abouts = () => {
+  return (
+    <div>abouts</div>
+  );
+}
+
+export default abouts;
---
+++ src/container/Testimonials/Testimonials.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./Testimonials.scss"
+
+const testimonials = () => {
+  return (
+    <div>testimonials</div>
+  );
+}
+
+export default testimonials;
